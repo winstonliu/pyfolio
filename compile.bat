a6017db5f942e99c0f1808b5eef9575f77d8c73a
Winston Liu <winston.liu@mail.utoronto.ca>
2020-05-02 15:27:47 -0700

    Updating compiliation script

--- compile.bat
+++ compile.bat
@@ -2,3 +2,4 @@
 pyside2-uic folio.ui > ui_folio.py
 pyside2-uic settings.ui > ui_settings.py
 pyside2-rcc -o resources.py resources.qrc
+pyinstaller --onefile --icon=icons\icon.ico -w -n folio main.py
